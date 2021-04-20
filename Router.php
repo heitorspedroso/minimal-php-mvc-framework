1e9128cbadc2ff830993ac0a150739540de8b596
Heitor <heitorspedroso@gmail.com>
2021-04-20 10:10:32 -0300

    Core 1.0

---
+++ Router.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\core;
+
+use app\core\exception\NotFoundException;/**
+ * Class Router
+ * @author Heitor Sousa <heitorspedroso@gmail.com>
+ *@package app\core
+ */
+class Router {
+
+	public Request $request;
+	public Response $response;
+	protected array $routes = [];
+
+	/**
+	 * Router constructor.
+	 *
+	 * @param Request $request
+	 */
+	public function __construct( Request $request, Response $response) {
+		$this->request = $request;
+		$this->response = $response;
+	}
+
+
+	public function get($path, $callback)
+	{
+		$this->routes['get'][$path] = $callback;
+	}
+
+	public function post($path, $callback)
+	{
+		$this->routes['post'][$path] = $callback;
+	}
+
+	public function resolve()
+	{
+		$path = $this->request->getPath();
+		$method = $this->request->method();
+		$callback  = $this->routes[$method][$path] ?? false;
+		if($callback === false){
+			throw new NotFoundException();
+		}
+
+		if(is_string($callback)){
+			return Application::$app->view->renderView($callback);
+		}
+
+		if (is_array($callback)) {
+
+			/**
+			 * @var \app\core\Controller $controller
+			 */
+			$controller = new $callback[0];
+			$controller->action = $callback[1];
+			Application::$app->controller = $controller;
+			$middlewares = $controller->getMiddlewares();
+			foreach ($middlewares as $middleware) {
+				$middleware->execute();
+			}
+			$callback[0] = $controller;
+		}
+		return call_user_func($callback, $this->request, $this->response);
+
+	}
+}
---
+++ form/TextareaField.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace app\core\form;
+
+
+class TextareaField extends BaseField {
+
+	public function renderInput(): string {
+		return sprintf('<textarea name="%s" class="form-control%s" id="%s">%s</textarea>',
+			$this->attribute,
+			$this->model->hasError($this->attribute) ? ' is-invalid' : '',
+			$this->attribute,
+			$this->model->{$this->attribute},
+		);
+	}
+}
